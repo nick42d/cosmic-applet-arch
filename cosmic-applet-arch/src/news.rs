d8cd1f8004cdff9fbd1ab607d642efec64418f56
Nick Dowsett <nickdowsett42@gmail.com>
2025-02-07 07:17:00 +0800

    Change module structure, add deps

---
+++ cosmic-applet-arch/src/news.rs
@@ -0,0 +1,5 @@
+//! API for feature to fetch latest news from arch RSS feed.
+
+/// To avoid displaying all news, we need to know when the system was last
+/// updated. Only show news later than that.
+mod latest_update {}
