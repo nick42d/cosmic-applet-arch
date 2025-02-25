53c1eb191745870daf0d88a2cacb37571d239310
Nick Dowsett <nickdowsett42@gmail.com>
2025-02-25 21:46:22 +0800

    WIP - get_last_updated tests

--- cosmic-applet-arch/src/news.rs
+++ cosmic-applet-arch/src/news.rs
@@ -9,6 +9,7 @@ mod latest_update {
     use anyhow::{anyhow, bail, Context};
     use chrono::{DateTime, Local};
     use directories::ProjectDirs;
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
     const PACMAN_LOG_PATH: &str = "/var/log/pacman.log";
     const LOCAL_LAST_READ_PATH: &str = "last_read";
@@ -16,7 +17,8 @@ mod latest_update {
     #[cfg_attr(test, mockall::automock)]
     trait ArchInstallation {
         async fn get_pacman_log(&self) -> std::io::Result<String>;
-        async fn get_local_update_timestamp_file(&self) -> std::io::Result<String>;
+        /// Note - should be provided in RW mode.
+        async fn get_local_update_timestamp_file(&self) -> std::io::Result<tokio::fs::File>;
     }
 
     struct Arch;
@@ -24,15 +26,17 @@ mod latest_update {
         async fn get_pacman_log(&self) -> std::io::Result<String> {
             tokio::fs::read_to_string(PACMAN_LOG_PATH).await
         }
-        async fn get_local_update_timestamp_file(&self) -> std::io::Result<String> {
+        async fn get_local_update_timestamp_file(&self) -> std::io::Result<tokio::fs::File> {
             let proj_dirs = ProjectDirs::from("com", "nick42d", "cosmic-applet-arch").unwrap();
-            tokio::fs::read_to_string(
-                proj_dirs
-                    .data_local_dir()
-                    .to_path_buf()
-                    .join(LOCAL_LAST_READ_PATH),
-            )
-            .await
+            tokio::fs::File::options()
+                .write(true)
+                .open(
+                    proj_dirs
+                        .data_local_dir()
+                        .to_path_buf()
+                        .join(LOCAL_LAST_READ_PATH),
+                )
+                .await
         }
     }
     pub async fn get_latest_update(
@@ -52,26 +56,30 @@ mod latest_update {
             (Err(e1), Err(e2)) => bail!("Errors determining last update, {e1}, {e2}"),
         }
     }
-    pub async fn set_local_last_read(datetime: DateTime<Local>) -> anyhow::Result<()> {
-        let proj_dirs = ProjectDirs::from("com", "nick42d", "cosmic-applet-arch")
-            .context("Error determining local data directory")?;
-        tokio::fs::write(
-            proj_dirs
-                .data_local_dir()
-                .to_path_buf()
-                .join(LOCAL_LAST_READ_PATH),
-            datetime.to_rfc3339(),
-        )
-        .await
-        .context("Error writing last read to disk")
+    pub async fn set_local_last_read(
+        platform: &impl ArchInstallation,
+        datetime: DateTime<Local>,
+    ) -> anyhow::Result<()> {
+        let mut handle = platform
+            .get_local_update_timestamp_file()
+            .await
+            .context("Error opening last read file")?;
+        handle
+            .write_all(datetime.to_rfc3339().as_bytes())
+            .await
+            .context("Error writing last read to disk")
     }
     async fn get_local_last_read(
         platform: &impl ArchInstallation,
     ) -> anyhow::Result<DateTime<Local>> {
-        let last_read_string = platform
+        let mut last_read_string = String::new();
+        platform
             .get_local_update_timestamp_file()
             .await
-            .context("Error reading last read file")?;
+            .context("Error opening last read file")?
+            .read_to_string(&mut last_read_string)
+            .await
+            .context("Error converting last read file to string")?;
         let date_time = DateTime::parse_from_rfc3339(&last_read_string)
             .context("Error parsing last read file")?;
         Ok(DateTime::<Local>::from(date_time))
@@ -115,17 +123,21 @@ mod latest_update {
                 .returning(|| Ok(include_str!("../test/pacman.log").to_string()));
             assert_eq!(
                 get_latest_pacman_update(&mock).await.unwrap(),
-                chrono::DateTime::<chrono::Local>::from(chrono::DateTime::UNIX_EPOCH)
+                chrono::FixedOffset::east_opt(8 * 60 * 60)
+                    .unwrap()
+                    .with_ymd_and_hms(2025, 2, 5, 22, 2, 13)
+                    .unwrap()
             );
         }
         #[tokio::test]
         async fn test_get_latest_local_update_mock() {
             let mut mock = MockArchInstallation::new();
             mock.expect_get_local_update_timestamp_file()
-                .returning(|| Ok("03 Feb 2025 11:24:25 +0000".to_string()));
+                .returning(|| Ok("2025-02-03T11:24:25+08:00".to_string()));
             assert_eq!(
                 get_local_last_read(&mock).await.unwrap(),
-                chrono::Utc
+                chrono::FixedOffset::east_opt(8 * 60 * 60)
+                    .unwrap()
                     .with_ymd_and_hms(2025, 2, 3, 11, 24, 25)
                     .unwrap()
             );
