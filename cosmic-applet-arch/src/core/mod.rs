d505df16f2824f9f118f470586e32c6802382302
Nick Dowsett <nickdowsett42@gmail.com>
2025-04-30 17:43:15 +0800

    Docs for proj_dirs

--- cosmic-applet-arch/src/core/mod.rs
+++ cosmic-applet-arch/src/core/mod.rs
@@ -3,6 +3,7 @@
 use directories::ProjectDirs;
 pub mod localization;
 
+/// ProjectDirs with the correct values for this application
 pub fn proj_dirs() -> Option<ProjectDirs> {
     ProjectDirs::from("com", "nick42d", "cosmic-applet-arch")
 }
