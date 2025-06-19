be5610767dc2d664c1b39e868eb8024ae866c27e
Nick Dowsett <nickdowsett42@gmail.com>
2025-06-19 11:05:09 +0800

    Fix - incorrect feature flag

--- cosmic-applet-arch/src/news.rs
+++ cosmic-applet-arch/src/news.rs
@@ -76,7 +76,7 @@ impl<T, W, E> WarnedResult<T, W, E> {
             WarnedResult::Err(e) => WarnedResult::Err(e),
         }
     }
-    #[cfg(not(feature = "mock-api"))]
+    #[cfg(feature = "mock-api")]
     pub fn is_ok(&self) -> bool {
         matches!(self, WarnedResult::Ok(_))
     }
